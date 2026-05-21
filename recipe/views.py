7b7e256efa13f22d4a555542543958f0c1780106
AlexMelnyk <zidder007@gmail.com>
2026-05-21 14:09:38 +0300

    add main view

--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,6 @@
+from django.shortcuts import render
+from .models import Recipe
 
+def main(request):
+    latest_recipes = Recipe.objects.all().order_by('-created_at')[:5]
+    return render(request, 'recipe/main.html', {'recipes': latest_recipes})
