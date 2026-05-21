c4ec43aece3e0402e932420bfd875c042978d326
AlexMelnyk <zidder007@gmail.com>
2026-05-21 14:49:35 +0300

    add setup for testing

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Category, Recipe
 
-# Create your tests here.
+class RecipeViewsTestCase(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name="Тестова категорія")
+        
+        for i in range(6):
+            Recipe.objects.create(
+                title=f"Рецепт №{i}",
+                description=f"Опис тестового рецепта {i}",
+                instructions="Крок 1. Крок 2.",
+                ingredients="Тестові інгредієнти",
+                category=self.category
+            )
+
+   
